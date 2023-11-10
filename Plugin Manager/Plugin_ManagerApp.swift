ff9ac6d02cd273f2b44c6f71e8683744a41ffed9
carlca <carl.caulkett@gmail.com>
2023-11-10 23:38:32 +0000

    Initial Commit

---
+++ Plugin Manager/Plugin_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plugin_ManagerApp.swift
+//  Plugin Manager
+//
+//  Created by Carl Caulkett on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Plugin_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Plugin ManagerUITests/Plugin_ManagerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Plugin_ManagerUITestsLaunchTests.swift
+//  Plugin ManagerUITests
+//
+//  Created by Carl Caulkett on 10/11/2023.
+//
+
+import XCTest
+
+final class Plugin_ManagerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
